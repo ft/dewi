0e938d9c5a4249dac5b87383bad9404100f68d02
Frank Terbeck <ft@bewatermyfriend.org>
2010-07-13 14:29:22 +0200

    Add script stubs
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ bin/dewi.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+# Copyright (c) 2010
+# Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
+# Terms for redistribution and use can be found in `LICENCE'.
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Copyright (c) 2010
+# Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
+# Terms for redistribution and use can be found in `LICENCE'.
