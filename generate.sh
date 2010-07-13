250c84343de8f0d0c0cf04d5b913b3e34b5bf650
Frank Terbeck <ft@bewatermyfriend.org>
2010-07-13 15:34:09 +0200

    Implement generate.sh
    Also adds a few copyright headers.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- generate.sh
+++ generate.sh
@@ -2,3 +2,73 @@
 # Copyright (c) 2010
 # Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
 # Terms for redistribution and use can be found in `LICENCE'.
+
+. ./lib/shell.sh
+
+basedir_unres=..
+posix_sh_unres=sh
+perl_unres=perl
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        sh=*)
+            posix_sh_unres=${1#sh=}
+            ;;
+        perl=*)
+            perl_unres=${1#perl=}
+            ;;
+        basedir=*)
+            basedir_unres=${1#basedir=}
+            ;;
+        *)
+            printf 'unknown parameter: `%s'\''\n' "$1"
+            printf 'See the `README'\'' file for details.\n'
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+die_not_found() {
+    rv=$1
+    name=$2
+    unres_value=$3
+
+    [ "$rv" = 0 ] && return 0
+    die '`'"$name"\'' not found: '"$unres_value"
+    # does not return
+}
+
+basedir=$(resolv_path "$basedir_unres")
+die_not_found "$?" basedir "$basedir_unres"
+posix_sh=$(find_binary "$posix_sh_unres")
+die_not_found "$?" posix_sh "$posix_sh_unres"
+perl=$(find_binary "$perl_unres")
+die_not_found "$?" perl "$perl_unres"
+bindir=$(resolv_path "./bin")
+die_not_found "$?" bindir "./bin"
+libdir=$(resolv_path "./lib")
+die_not_found "$?" libdir "./lib"
+
+printf '%s\n' "Configuration:"
+printf '  perl:     %s\n' "$perl"
+printf '  posix_sh: %s\n' "$posix_sh"
+printf '  basedir:  %s\n' "$basedir"
+printf '%s\n' "dewi locations:"
+printf '  bindir:   %s\n' "$bindir"
+printf '  libdir:   %s\n' "$libdir"
+
+__generate() {
+    "$perl" -npe '
+        s!\@\@BASEDIR\@\@!'"$basedir"'!;
+        s!\@\@BINDIR\@\@!'"$bindir"'!;
+        s!\@\@LIBDIR\@\@!'"$libdir"'!;
+    '
+}
+
+printf '%s\n' "Generating files:"
+for file in "$libdir"/*."in"; do
+    gen_file=${file%.in}
+    printf '  %s\n' "$gen_file"
+    __generate < "$file" > "$gen_file"
+done
---
+++ lib/shell.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Copyright (c) 2010
+# Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
+# Terms for redistribution and use can be found in `LICENCE'.
+
+die() {
+    printf '%s\n' "$1"
+    exit 1
+}
+
+find_binary() {
+    prog="$1"
+    [ -z "$prog" ] && return 1
+
+    case "$prog" in
+        /*)
+            if [ -x "$prog" ]; then
+                printf '%s' "$dir/$prog"
+                return 0
+            fi
+            ;;
+    esac
+
+    ret=1
+    oifs="$IFS"
+    IFS=:
+    for dir in $PATH; do
+        [ -z "$dir" ] && continue
+        if [ -x "$dir/$prog" ]; then
+            printf '%s' "$dir/$prog"
+            ret=0
+            break
+        fi
+    done
+
+    IFS="$oifs"
+    unset oifs
+    return "$ret"
+}
+
+resolv_path() {
+    [ -d "$1" ] || return 1
+    (
+        cd "$1" || return 1
+        printf '%s' "$(pwd)"
+        return 0
+    )
+    return $?
+}
