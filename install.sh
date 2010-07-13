48b57482858a33fe65c557721a82d08aa6491901
Frank Terbeck <ft@bewatermyfriend.org>
2010-07-13 16:18:13 +0200

    Implement install.sh
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@
 # Copyright (c) 2010
 # Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
 # Terms for redistribution and use can be found in `LICENCE'.
+
+. ./lib/shell.sh
+if [ ! -r ./lib/genshell.sh ]; then
+    die 'Please run generate.sh first. See README for details.'
+    exit 1
+fi
+. ./lib/genshell.sh
+
+cp lib/parent.mk "$basedir"/Makefile
