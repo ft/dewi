a5846863e89ca9a26ef2f2495133afe9a79f90c1
Frank Terbeck <ft@bewatermyfriend.org>
2010-10-08 20:34:07 +0200

    Infrastructure for archiving actions
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ bin/rmrf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Copyright (c) 2010
+# Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
+# Terms for redistribution and use can be found in `LICENCE'.
+
+set +x
+rc=0
+for i in "$@"; do
+    command rm -Rf "$i" || rc=1
+done
+exit "$rc"
