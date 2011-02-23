2408afa332e5f2ae93a9f2388a7146daf6c03ac0
Frank Terbeck <ft@bewatermyfriend.org>
2011-02-23 23:55:57 +0100

    Updating dates
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (c) 2010
+# Copyright (c) 2010-2011
 # Frank Terbeck <ft@bewatermyfriend.org>, All rights reserved.
 # Terms for redistribution and use can be found in `LICENCE'.
 
